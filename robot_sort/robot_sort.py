71fdf25420199ae6ab88e0399f982db981c43dc9
UnknownMonk <smodcartoon@gmail.com>
2019-06-07 13:02:56 -0400

    test passing sort done

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -81,11 +81,13 @@ class SortingRobot:
         Turn on the robot's light
         """
         self._light = "ON"
+
     def set_light_off(self):
         """
         Turn off the robot's light
         """
         self._light = "OFF"
+
     def light_is_on(self):
         """
         Returns True if the robot's light is on and False otherwise.
@@ -97,14 +99,47 @@ class SortingRobot:
         Sort the robot's list.
         """
         # Fill this out
-        pass
+        # first set the light to on then start the loop
+        self.set_light_on()
+
+        while self.light_is_on() is True:
+
+            # to exit the loop you must turn the light off to indicate false
+            self.set_light_off()
+
+            # start checking if the move right function is true in a while loop
+            while self.can_move_right() is True:
+               # swap the item and move to the right
+                self.swap_item()
+                self.move_right()
+
+                # check if the compare_item function is returning 1 and if it is swap then move left and and drop item move to the right and then turn the light on.
+                if self.compare_item() == 1:
+                    self.swap_item()
+                    self.move_left()
+                    self.swap_item()
+                    self.move_right()
+                    self.set_light_on()
+
+                # otherwise move left drop it and keep going
+                else:
+                    self.move_left()
+                    self.swap_item()
+                    self.move_right()
+
+            # as long as the light is on start a loop to contuine to move left.
+            if self.light_is_on():
+                while self.can_move_left():
+                    self.move_left()
 
 
+# okay that is all folks..!
 if __name__ == "__main__":
     # Test our your implementation from the command line
     # with `python robot_sort.py`
 
-    l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
+    l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1,
+         45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
 
     robot = SortingRobot(l)
 
